f28fdc9669d1f1560db9a0b01652973dba6cc299
Thiago Buttler Paulino - 823129867 <823129867@ulife.com.br>
2025-02-25 20:13:50 -0300

    Aula

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
 
 console.log("OI gente")
+
+let x = 7
+const y = 3
+console.log(x)
+console.log(y)
+
+console.log("oi " + x)
+
+"Conversão de tipos de dados"
+let texto = "123"
+let num = parseInt(texto)
+console.log(num+x)
